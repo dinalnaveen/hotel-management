59e08d34da0b7d3e9502ef8bb6552c93d44d1769
dinalnaveen <dinalnaveen@gmail.com>
2023-10-03 14:42:58 +0530

    Add CSS

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,31 +1,38 @@
 import React from 'react'
 import Image from 'next/image'
+import styles from './header.module.css'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 
 export default function Header() {
   return (
-    <div className='flex justify-between items-center p-1 pl-7 pr-7 shadow-sm'>
-        <div>
+    <div className='flex justify items-center p-1 pl-10 pr-7 shadow-sm'>
+        <div className=' absolute'>
             <Image src='/logo.png' alt='logo'
-            width={50} height={50}/>
+            width={90} height={70}/>
         </div>
-        <ul className='flex gap-10'>
-            <li className='text-[14] text-[#545455] hover:text-[#07AF56] cursor-pointer'>Home</li>
-            <li className='text-[14] text-[#545455] hover:text-[#07AF56] cursor-pointer'>Packages</li>
-            <li className='text-[14] text-[#545455] hover:text-[#07AF56] cursor-pointer'>Gallery</li>
-            <li className='text-[14] text-[#545455] hover:text-[#07AF56] cursor-pointer'>Room</li>
-            <li className='text-[14] text-[#545455] hover:text-[#07AF56] cursor-pointer'>About Us</li>
-            <li className='text-[14] text-[#545455] hover:text-[#07AF56] cursor-pointer'>Contact Us</li>
+        <ul className='flex gap-10 ml-[220px]'>
+            <li className={`${styles.menuItem} cursor-pointer hover:scale-105`}>Home</li>
+            <li className={`${styles.menuItem} cursor-pointer hover:scale-105`}>Gallery</li>
+            <li className={`${styles.menuItem} cursor-pointer hover:scale-105`}>Offers</li>
+            <li className={`${styles.menuItem} cursor-pointer hover:scale-105`}>About Us</li>
+            <li className={`${styles.menuItem} cursor-pointer hover:scale-105`}>FAQs</li>
         </ul>
-        <div className='flex gap-10 items-center '>
-        <Image src='/flag.jpg' alt='logo'
+
+        <div className='flex gap-10 items-center ml-[460px] mr-[60px] '>
+        <Image src='/flag2.png' alt='logo'
             width={40} height={30}/>
-            <h3 className='text-[14] text-[#545455] cursor-pointer'>USD</h3>
-            <button className="bg-transparent hover:bg-[#07AF56] text-[#07AF56] font-semibold hover:text-white py-2 px-8 border border-[#07AF56] hover:border-transparent rounded">
+            <h3 className={`${styles.icon} cursor-pointer hover:scale-105`} >USD</h3>
+            <button className={`${styles.btn} bg-transparent  py-2 px-8 border hover:border-transparent rounded`}>
             Login
             </button>
             <i className="fi fi-rs-shopping-cart"></i>
+            <div className=' absolute right-0 mr-[60px]'>
+            <AiOutlineShoppingCart className="w-6 h-6 text-[#545455cb] hover:scale-110 cursor-pointer hover:text-[#545455]"/>
+            </div>
+
         </div>
+        
     </div>
   )
 }
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { BiSolidDownArrow } from 'react-icons/bi';
 import { IoFilterSharp } from 'react-icons/io5';
+import styles from './hero.module.css'
 
 
 export default function Hero() {
@@ -11,48 +12,48 @@ export default function Hero() {
       <Image src='/hero2.png' alt='hero-image' width={800} height={200} className='w-full absolute mt-[-70] ' />
       
       <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center mt-[230px]'>
-        <h2 className='text-[40px] font-semibold text-white'>Welcome to</h2>
-        <h2 className='text-[100px] font-extrabold text-white'>Our Villas</h2>
+        <h2 className={styles.titleLine1}>Welcome to</h2>
+        <h2 className={styles.titleLine2}>Our Villas</h2>
       </div>
 
       <div className='flex justify-center  '>
-        <div className='absolute flex items-center gap-9 bg-white mt-[480px] p-[20px] px-[30px] border-[2px] border-[#07AF56] rounded-md'>
+        <div className={`${styles.bookingWidget} absolute flex items-center gap-9 mt-[480px] p-[20px] px-[30px] border-[2px]  rounded-md`}>
         
       <div className='z-10 mt-50  top-0 left-0  '>
-        <h2 className='text-[12] text-[#07AF56]'>Search</h2>
+        <h2 className={styles.bookingTitle}>Search</h2>
         <div className='flex gap-2 items-center'>
-        <AiOutlineSearch className="w-6 h-6 text-[#545455]" />
-        <input type="text" placeholder='Enter a destination' className='text-[#545455] w-[160px] py-[5px] outline-none placeholder-[#545455] placeholder-opacity-80'  />
+        <AiOutlineSearch className="w-6 h-6 text-[#545455cb]" />
+        <input type="text" placeholder='Enter a destination' className={`${styles.bookingText}  w-[160px] py-[5px] outline-none placeholder-[#545455cd] placeholder-opacity-80  `}/>
         </div> 
       </div>
 
       <div className='z-10 mt-50  top-0 left-0  '>
-        <h2 className='text-[12] text-[#07AF56]'>Check-In</h2>
+        <h2 className={styles.bookingTitle}>Check-In</h2>
         <div className='flex gap-2 items-center'>
-          <button className='flex gap-2 items-center text-[#545455] text-opacity-80 py-[5px] text-[16px]'>Select date <BiSolidDownArrow className="w-3 h-3 text-[#545455]" /></button>
-        </div> 
-      </div>
-
-
-      <div className='z-10 mt-50  top-0 left-0  '>
-        <h2 className='text-[12] text-[#07AF56]'>Check-Out</h2>
-        <div className='flex gap-2 items-center'>
-          <button className='flex gap-2 items-center text-[#545455] text-opacity-80 py-[5px] text-[16px]'>Select date <BiSolidDownArrow className="w-3 h-3 text-[#545455]" /></button>
-        </div> 
-      </div>
-
-      <div className='z-10 mt-50  top-0 left-0  '>
-        <h2 className='text-[12] text-[#07AF56]'>Adult</h2>
-        <div className='flex gap-2 items-center'>
-          <button className='flex gap-2 items-center text-[#545455] text-opacity-80 py-[5px] text-[16px]'>0<BiSolidDownArrow className="w-3 h-3 text-[#545455]" /></button>
+          <button className={`${styles.bookingText} flex gap-2 items-center  text-opacity-80 py-[5px] `}>Select date <BiSolidDownArrow className="w-3 h-3 text-[#545455cb]" /></button>
         </div> 
       </div>
 
 
       <div className='z-10 mt-50  top-0 left-0  '>
-        <h2 className='text-[12] text-[#07AF56]'>Child</h2>
+        <h2 className={styles.bookingTitle}>Check-Out</h2>
         <div className='flex gap-2 items-center'>
-          <button className='flex gap-2 items-center text-[#545455] text-opacity-80 py-[5px] text-[16px]'>0<BiSolidDownArrow className="w-3 h-3 text-[#545455]" /></button>
+          <button className={`${styles.bookingText} flex gap-2 items-center  text-opacity-80 py-[5px] `}>Select date <BiSolidDownArrow className="w-3 h-3 text-[#545455cb]" /></button>
+        </div> 
+      </div>
+
+      <div className='z-10 mt-50  top-0 left-0  '>
+        <h2 className={styles.bookingTitle}>Adult</h2>
+        <div className='flex gap-2 items-center'>
+          <button className={`${styles.bookingText} flex gap-2 items-center  text-opacity-80 py-[5px] `}>0<BiSolidDownArrow className="w-3 h-3 text-[#545455cb]" /></button>
+        </div> 
+      </div>
+
+
+      <div className='z-10 mt-50  top-0 left-0  '>
+        <h2 className={styles.bookingTitle}>Child</h2>
+        <div className='flex gap-2 items-center'>
+          <button className={`${styles.bookingText} flex gap-2 items-center  text-opacity-80 py-[5px] `}>0<BiSolidDownArrow className="w-3 h-3 text-[#545455cb]" /></button>
         </div> 
       </div>
 
@@ -61,7 +62,7 @@ export default function Hero() {
       </div>
 
       <div>
-      <button className="bg-[#07AF56] hover:bg-[#1E6E44] text-white font-bold py-[14px] px-4 rounded">
+      <button className={`${styles.customButton}   py-[14px] px-4 rounded`}>
         Check Availability
       </button>
       </div>
